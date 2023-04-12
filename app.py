ef3ac34ea6ca6324691f53a19dbb7d98d5565e5b
Yoder. Austin <Austin.Yoder@supsc.org>
2023-04-12 16:07:28 -0500

    Changes

--- app.py
+++ app.py
@@ -1,26 +1,15 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template
 import json
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
 
 @app.route('/area_breakdown')
-def area_breakdown():
+def area_breakdown_page():  # <-- change this line
     return render_template('area_breakdown.html')
-
-@app.route('/area_data', methods=['GET'])
-def area_data():
-    with open('sample_data.json', 'r') as file:
-        data = json.load(file)
-    return jsonify(data)
-
-@app.route('/area_breakdown')
-def area_breakdown():
-    return render_template('area_breakdown.html')
-
 
 if __name__ == '__main__':
     app.run(debug=True)
